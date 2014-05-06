a24fb4a9adf6e4de51d50929f9e730dfd14647d6
Carlos Ramirez <carlos.ramirez@mayan-tech.com>
2014-05-06 15:46:35 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Songs::Application.config.secret_key_base = 'a9ebcf2e42fbc26f53de6828f9a7532305deeb72fa194bcdcf0a949abc1cd9cb7946b6d928e25ff939d57b0556d4a8a0d7021c30dc5ecdfc8e5234da30c35d3c'
